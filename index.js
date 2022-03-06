d6b0e50ff6f738b9bb455efb75ae8c2ffd5bcbbd
creativename64 <75571388+creativename64@users.noreply.github.com>
2022-03-06 08:20:21 -0500

    testing update

--- index.js
+++ index.js
@@ -13,29 +13,28 @@ client.on("messageCreate", function(message) {
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    if (command === "ping") {
+    if (command === "pingpinpinpinpinpinpinpinpinpinping") {
         message.reply(`Pong!`);
     }
     if (command === "draa") {
         message.reply('draa.')
     }
-    if (command === "gaelansucks") {
+    if (command === "gaelansucksahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh") {
         member.kick();
     }
-    if (command === "start") {
-        message.reply('started thread life support test')
+    if (command === "mylittlebitchisepicnowitshallstartitsmission") {
+        message.delete()
+            .then(() => console.log('Deleted message'))
+            .catch(err => console.log(err))
+
+        message.channel.send('will be back')
             // Runs 5 times, with values of step 0 through 4.
-            setTimeout(() => {  message.reply('!start'); }, 79200000);
+            setTimeout(() => {  message.reply('threadmylittlebitchisepicnowitshallstartitsmission'); }, 72000000);
 
 
     }
     if (command === "help") {
-        message.reply('Heres what "ThreadKeeper" can do' +
-            ' please note the prefix of all commands is "thread"' +
-            ' threadstart will start an infinite loop that sends messages every now and then keeping a thread alive' +
-            ' threadping responds with pong (testing)' +
-            ' threaddraa responds with draa (derogatory)' +
-            ' version 1.0')
+        message.reply('commands are currently disabled')
     }
 
 
