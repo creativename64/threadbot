7b27dbd384a3c22a5fa37c691102849bc8531f19
creativename64 <75571388+creativename64@users.noreply.github.com>
2022-03-06 09:54:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 const prefix = "thread";
 
-.listen(process.env.PORT || 5000)
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+app.listen(port, host, function() {
+    console.log("Server started.......");
+  });
 
 client.on("messageCreate", function(message) {
 
