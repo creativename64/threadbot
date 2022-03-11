0f01644c61e810bbe4aff513bdbb686936fcdd57
creativename64 <75571388+creativename64@users.noreply.github.com>
2022-03-11 16:40:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("./config.json");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 
-const prefix = "thread";
+const prefix = "thisbotsucks";
 
 
 client.on("messageCreate", function(message) {
@@ -14,28 +14,33 @@ client.on("messageCreate", function(message) {
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
-    if (command === "pingpinpinpinpinpinpinpinpinpinping") {
-        message.reply(`Pong!`);
+    if (command === "ping") {
+        message.channel.send(`Pong!`);
+        message.delete()
     }
     if (command === "draa") {
-        message.reply('draa.')
+        message.channel.send('draa.')
+        message.delete()
     }
     if (command === "gaelansucksahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh") {
         member.kick();
     }
     if (command === "mylittlebitchisepicnowitshallstartitsmission") {
+
         message.delete()
             .then(() => console.log('Deleted message'))
             .catch(err => console.log(err))
 
+
         message.channel.send('will be back')
             // Runs 5 times, with values of step 0 through 4.
-            setTimeout(() => {  message.reply('threadmylittlebitchisepicnowitshallstartitsmission'); }, 72000000);
+            setTimeout(() => {  message.channel.send('thisbotsucksmylittlebitchisepicnowitshallstartitsmission'); }, 5000);
 
 
     }
     if (command === "help") {
-        message.reply('commands are currently disabled')
+        message.delete()
+        message.channel.send('commands are currently disabled')
     }
 
 
