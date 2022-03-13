fcf6b109b2b4b45906c90d9e40e149593e81e341
creativename64 <75571388+creativename64@users.noreply.github.com>
2022-03-13 14:50:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 // instantiate server
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
 
-const prefix = "beta";
+const prefix = "thread";
 
 
 client.on("messageCreate", function(message) {
@@ -19,70 +19,82 @@ client.on("messageCreate", function(message) {
 
     if (!message.content.startsWith(prefix)) return;
 
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
-    if (command === "ping") {
-        message.channel.send(`Pong!`);
-        message.delete()
+    cheese = message.member.id
+    console.log(cheese)
+    if (cheese === "449162438605012994") {
+        console.log('draa is back at it')
     }
-    if (command === "draa") {
-        message.channel.send('draa.')
-        message.delete()
-    }
-
-
-
-    if (command === "start") {
-
-        cheese = message.member.id
-        console.log(cheese)
-
-        message.delete()
-            .then(() => console.log('Deleted message'))
-            .catch(err => console.log(err))
-
-        if (cheese === "674733684787773440") {
-            message.channel.send('started - Creative Name#4801')
-            // Runs 5 times, with values of step 0 through 4.
-            setTimeout(() => {
-                message.channel.send('betastart');
-            }, 36000000);
-
+    else {
+        const commandBody = message.content.slice(prefix.length);
+        const args = commandBody.split(' ');
+        const command = args.shift().toLowerCase();
+        if (command === "ping") {
+            message.channel.send(`Pong!`);
+            message.delete()
         }
-        else {
-            if (cheese === "949834414135836732") {
-                message.channel.send('started')
+        if (command === "draa") {
+            cheese = message.member.id
+            console.log(cheese)
+
+            message.channel.send('draa.')
+            message.delete()
+        }
+
+
+
+        if (command === "start") {
+
+
+
+            message.delete()
+                .then(() => console.log('Deleted message'))
+                .catch(err => console.log(err))
+
+            if (cheese === "674733684787773440") {
+                message.channel.send('started - Creative Name#4801')
                 // Runs 5 times, with values of step 0 through 4.
                 setTimeout(() => {
-                    message.channel.send('betastart');
-                }, 36000000);
+                    message.channel.send('threadstart');
+                }, 5000 * 60 * 60);
 
             }
             else {
-                if (cheese === "951967161956585563") {
+                if (cheese === "949834414135836732") {
                     message.channel.send('started')
                     // Runs 5 times, with values of step 0 through 4.
                     setTimeout(() => {
-                        message.channel.send('betastart');
-                    }, 36000000);
+                        message.channel.send('threadstart');
+                    }, 5000 * 60 * 60);
 
                 }
                 else {
-                    message.channel.send('you dont have the perms to send that')
+                    if (cheese === "951967161956585563") {
+                        message.channel.send('started')
+                        // Runs 5 times, with values of step 0 through 4.
+                        setTimeout(() => {
+                            message.channel.send('threadstart');
+                        }, 5000 * 60 * 60);
+
+                    }
+                    else {
+                        message.channel.send('you dont have the perms to send that')
+                    }
                 }
             }
+
+
+
+
+
+        }
+        if (command === "help") {
+            message.delete()
+            message.channel.send('Thread Bot (version 1.1) command list \nthreadstart = starts the support system for one hour (testing) (please note that this command requires be to authorized inorder to fuction) \nthreadping and threaddraa = responds with a message to see if the bot is still working \nif you have any ideas on how to improve the bot feel free to send them to Creative Name#4801' )
         }
 
-
-
-
-
     }
-    if (command === "help") {
-        message.delete()
-        message.channel.send('Thread Bot (version 1.1) command list \nthreadstart = starts the support system for ten hours (please note that this command requires be to authorized inorder to fuction) \nthreadping and threaddraa = responds with a message to see if the bot is still working \nif you have any ideas on how to improve the bot feel free to send them to Creative Name#4801' )
-    }
+
+
 
 
 
