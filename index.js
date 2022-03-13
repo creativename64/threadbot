6925691d84f55a12e925c85e0c1e0e7728e9aa55
creativename64 <75571388+creativename64@users.noreply.github.com>
2022-03-13 19:41:33 -0400

    2.0 update

--- index.js
+++ index.js
@@ -1,105 +1,192 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-let cheese;
-// server imports
-const express = require('express');
-const app = express();
-const port = 3000;
-// create main route
-app.get('/', (req, res) => res.send('Hello World!'));
-// instantiate server
-app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
-
-const prefix = "thread";
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
-client.on("messageCreate", function(message) {
-
-
-    if (!message.content.startsWith(prefix)) return;
-
-    cheese = message.member.id
-    console.log(cheese)
-    if (cheese === "449162438605012994") {
-        console.log('draa is back at it')
-    }
-    else {
-        const commandBody = message.content.slice(prefix.length);
-        const args = commandBody.split(' ');
-        const command = args.shift().toLowerCase();
-        if (command === "ping") {
-            message.channel.send(`Pong!`);
-            message.delete()
-        }
-        if (command === "draa") {
-            cheese = message.member.id
-            console.log(cheese)
-
-            message.channel.send('draa.')
-            message.delete()
-        }
-
-
-
-        if (command === "start") {
-
-
-
-            message.delete()
-                .then(() => console.log('Deleted message'))
-                .catch(err => console.log(err))
-
-            if (cheese === "674733684787773440") {
-                message.channel.send('started - Creative Name#4801')
-                // Runs 5 times, with values of step 0 through 4.
-                setTimeout(() => {
-                    message.channel.send('threadstart');
-                }, 5000 * 60 * 60);
-
-            }
-            else {
-                if (cheese === "949834414135836732") {
-                    message.channel.send('started')
-                    // Runs 5 times, with values of step 0 through 4.
-                    setTimeout(() => {
-                        message.channel.send('threadstart');
-                    }, 5000 * 60 * 60);
-
-                }
-                else {
-                    if (cheese === "951967161956585563") {
-                        message.channel.send('started')
-                        // Runs 5 times, with values of step 0 through 4.
-                        setTimeout(() => {
-                            message.channel.send('threadstart');
-                        }, 5000 * 60 * 60);
-
-                    }
-                    else {
-                        message.channel.send('you dont have the perms to send that')
-                    }
-                }
-            }
-
-
-
-
-
-        }
-        if (command === "help") {
-            message.delete()
-            message.channel.send('Thread Bot (version 1.1) command list \nthreadstart = starts the support system for one hour (testing) (please note that this command requires be to authorized inorder to fuction) \nthreadping and threaddraa = responds with a message to see if the bot is still working \nif you have any ideas on how to improve the bot feel free to send them to Creative Name#4801' )
-        }
-
-    }
-
-
-
-
-
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
+client.on('interactionCreate', async (interaction) => {
+    let cheese;
+    let chan;
+    if (!interaction.isCommand()) return;
 
-client.login(config.BOT_TOKEN);
+    if (interaction.commandName === 'draa') {
+        interaction.reply('draa.')
+    }
+
+    if (interaction.commandName === 'about') {
+        interaction.reply("ThreadBot v2.0 (pre-beta) by Creative Name #4801 (time intervals are planned but as of yet not in)\n~now with slash commands~ \n what does it mean " +
+            "by (admin)? commands that require admin requires a user to have administrator perms in order to run (or to be the bot creator" +
+            "\nthanks for using it <3 if you have any suggestions on the bot feel free to DM the creator")
+    }
+
+    if (interaction.commandName === 'update') {
+        interaction.reply("**ThreadBot v2.0 Update Log**" +
+            "\n~Moved all commands into slash commands" +
+            "\n-Removed the ping command" +
+            "\n~Removed the start command in favor of several that allow you to specify time increments" +
+            "\n~Updated PHP to a new colour to show the updates" +
+            "\n~Updated some packages in the back-end to fix a vulnerability" +
+            "\n~Re-did most of the code in the back end" +
+            "\n-Removed the Dara lock from the backend" +
+            "\n~changed hosting from repl.it to being in house")
+    }
+    if (interaction.commandName === 'perms') {
+        interaction.reply("some commands in ThreadBot require perms to run, not doing so if you lack the perms. " +
+            "what ThreadBot looks to for run these commands is if the user has the 'administrator' privilege " +
+            "please note there is also an exception of one user hard coded into the program")
+    }
+    if (interaction.commandName === 'betterthreadbotinfo') {
+        interaction.reply("betterthreadbot is an alternative bot to threadbot that does almost the same thing. betterthreadbot" +
+            " is not developed by the same person and ironically as of the 2.0 update does not have slash commands or the ability" +
+            " to pick how long the delay between messages is. However if you are having problems with threadbot it may resolve them" +
+            " you can add it here -> https://discord.com/api/oauth2/authorize?client_id=950547590590640158&permissions=534723950656&scope=bot" +
+            " I DO NOT TAKE RESPONSIBILITY FOR THE ACTIONS PERFORMED BY BETTER THREAD BOT")
+    }
+
+    if (interaction.commandName === 'start-1h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 1000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 1000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+        }
+    if (interaction.commandName === 'start-2h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 2000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 2000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+    if (interaction.commandName === 'start-3h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 3000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 3000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+    if (interaction.commandName === 'start-4h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 4000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 4000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+    if (interaction.commandName === 'start-8h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 8000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 8000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+    if (interaction.commandName === 'start-12h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 12000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 12000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+    if (interaction.commandName === 'start-20h') {
+        if (interaction.memberPermissions.has("ADMINISTRATOR")) {console.log("check")
+            interaction.reply("Thread Life Support has now begun <3")
+            setInterval(() => {interaction.channel.send("still alive")}, 20000*60*60)
+
+
+        }
+        else{
+            interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            cheese = interaction.member.user.id
+            if (cheese === '674733684787773440') {
+                console.log("check2")
+                interaction.reply("Thread Life Support has now begun <3")
+                setInterval(() => {interaction.channel.send("still alive")}, 20000*60*60)
+
+            }
+            else {interaction.reply("sorry but you don't have the perms to run this command. Look at /perms")
+            }
+        }
+    }
+
+});
+
+client.login(process.env.TOKEN);
---
+++ register.js
@@ -0,0 +1,68 @@
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const commands = [
+    {
+        name: 'draa',
+        description: 'application sanity test',
+    },
+    {
+        name: "start-1h",
+        description: 'starts the 1h cycle',
+    },
+    {
+        name: "about",
+        description: 'gives info about ThreadBots current version',
+    },
+    {
+        name: "update",
+        description: 'shows the changelog since the last major version'
+    },
+    {
+        name: "betterthreadbotinfo",
+        description: "displays info about the betterthreadbotinfo"
+    },
+    {
+        name: "perms",
+        description: 'displays info about perms',
+    },
+    {
+        name: "start-2h",
+        description: 'starts the 2h cycle',
+    },
+    {
+        name: "start-3h",
+        description: 'starts the 3h cycle',
+    },
+    {
+        name: "start-4h",
+        description: 'starts the 4h cycle',
+    },
+    {
+        name: "start-8h",
+        description: 'starts the 8h cycle',
+    },
+    {
+        name: "start-12h",
+        description: 'starts the 12h cycle',
+    },
+    {
+        name: "start-20h",
+        description: 'starts the 20h cycle - recommended',
+    },
+];
+
+const rest = new REST({version: '9'}).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(process.env.APP_ID), {
+            body: commands,
+        });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
